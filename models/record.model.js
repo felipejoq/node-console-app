3e3115e39d56f39e5cd22fd3f2917c56227db314
felipejoq <felipejofreq@gmail.com>
2024-01-21 00:45:13 -0300

    Se agrega ID al modelo de registros

--- models/record.model.js
+++ models/record.model.js
@@ -1,11 +1,16 @@
 export class Record {
 
   constructor(name, age, type, color, disease){
+    this.id = this.getId();
     this.name = name;
     this.age = age;
     this.type = type;
     this.color = color;
     this.disease = disease;
+  }
+
+  getId(){
+    return new Date().getTime();
   }
 
 }
