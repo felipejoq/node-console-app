87b9b8eec7543e44a4446632a104e279ffc5dc82
felipejoq <felipejofreq@gmail.com>
2024-01-21 10:13:31 -0300

    Mejora legibilidad index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import { RecordService } from "./services/records.service.js";
 
 const args = process.argv.slice(2);
-const operation = args[0]
-
-let inputs;
-if (args.length > 1)
-  inputs = args.slice(1);
+const [operation, ...inputs] = args;
 
 new RecordService().showResult(operation, inputs);
